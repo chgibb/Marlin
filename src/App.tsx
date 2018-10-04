54027ea67e186dc22177b0188ddba697b6dc8ed1
Chris Gibb <chris.gibb@outlook.com>
2018-10-04 16:29:54 -0400

    normalize formatting

--- src/App.tsx
+++ src/App.tsx
@@ -20,76 +20,76 @@ class AppState
 }
 
 class App extends React.Component {
-  public state : AppState = new AppState();
-  public render() {
-    return (
-      <Container>
-        <Header>
-        <div style={{ display: "flex", alignItems: "center" }} />
-        <SearchBox onSearchUpdated={this.onSearchUpdated} />
-        <a href={`https://github.com/chgibb`}>chgibb</a>
-        </Header>
-        <div className="root">
-          <Grid container spacing={24}>
-            <Grid item xs>
-            {
-              this.state.analyzedURL && !this.state.basicURLInfo ?
-              <h1>Analyzing {this.state.analyzedURL}</h1> : ""
-            }
-            {
-              this.state.analyzedURL && this.state.basicURLInfo ? 
-              <h1>{this.state.analyzedURL}</h1> : ""
-            }
-            {
-              this.state.basicURLInfo && this.state.basicURLInfo.remoteAddress ?
-              <RemoteAddressInfo remoteAddress={this.state.basicURLInfo.remoteAddress} /> : ""
-            }
-            {
-              this.state.basicURLInfo && this.state.basicURLInfo.screenShot ? 
-              <img src={"data:image/png;base64, "+this.state.basicURLInfo.screenShot} /> : ""
-            }
-            {
-              this.state.basicURLInfo && this.state.basicURLInfo.screenShot && this.state.wappalyzeResult === undefined ?
-              <p>Still analyzing</p> : ""
-            }
-            <div>{
-              this.state.wappalyzeResult && this.state.wappalyzeResult.applications ?
-              <div><p>Built With:</p><br />{(this.state.wappalyzeResult!.applications.map(item => {
-                if(!/\.svg/g.test(item.icon))
-                {
-                  return (
-                    <a href={item.website} target="_blank"><img key={item.icon} src={"https://github.com/AliasIO/Wappalyzer/raw/master/src/icons/"+item.icon}/></a>
-                  )
-                }
-                else
-                  return undefined;
-              }))}</div> : ""
+    public state : AppState = new AppState();
+    public render() {
+        return (
+        <Container>
+            <Header>
+                <div style={{ display: "flex", alignItems: "center" }} />
+                <SearchBox onSearchUpdated={this.onSearchUpdated} />
+                <a href={`https://github.com/chgibb`}>chgibb</a>
+            </Header>
+            <div className="root">
+                <Grid container spacing={24}>
+                    <Grid item xs>
+                    {
+                        this.state.analyzedURL && !this.state.basicURLInfo ?
+                        <h1>Analyzing {this.state.analyzedURL}</h1> : ""
+                    }
+                    {
+                        this.state.analyzedURL && this.state.basicURLInfo ? 
+                        <h1>{this.state.analyzedURL}</h1> : ""
+                    }
+                    {
+                        this.state.basicURLInfo && this.state.basicURLInfo.remoteAddress ?
+                        <RemoteAddressInfo remoteAddress={this.state.basicURLInfo.remoteAddress} /> : ""
+                    }
+                    {
+                        this.state.basicURLInfo && this.state.basicURLInfo.screenShot ? 
+                        <img src={"data:image/png;base64, "+this.state.basicURLInfo.screenShot} /> : ""
+                    }
+                    {
+                        this.state.basicURLInfo && this.state.basicURLInfo.screenShot && this.state.wappalyzeResult === undefined ?
+                        <p>Still analyzing</p> : ""
+                    }
+                    <div>{
+                        this.state.wappalyzeResult && this.state.wappalyzeResult.applications ?
+                        <div><p>Built With:</p><br />{(this.state.wappalyzeResult!.applications.map(item => {
+                            if(!/\.svg/g.test(item.icon))
+                            {
+                                return (
+                                    <a href={item.website} target="_blank"><img key={item.icon} src={"https://github.com/AliasIO/Wappalyzer/raw/master/src/icons/"+item.icon}/></a>
+                                )
+                            }
+                            else
+                                return undefined;
+                        }))}</div> : ""
 
-            }</div>
-            {
-              this.state.basicURLInfo && this.state.basicURLInfo.urls && this.state.basicURLInfo.urls.length > 0 ?
-              <div><p>Network Requests:</p><br />{(<NetworkRequestInfo urls={this.state.basicURLInfo.urls} />)}</div> : ""
-            }
-            </Grid>
-          </Grid>
-        </div>
-      </Container>
-    );
-  }
+                    }</div>
+                    {
+                        this.state.basicURLInfo && this.state.basicURLInfo.urls && this.state.basicURLInfo.urls.length > 0 ?
+                        <div><p>Network Requests:</p><br />{(<NetworkRequestInfo urls={this.state.basicURLInfo.urls} />)}</div> : ""
+                    }
+                  </Grid>
+                </Grid>
+              </div>
+          </Container>
+         );
+    }
 
-  public onSearchUpdated = async (text : string) => {
-    console.log(text);
-    this.setState({analyzedURL : text,basicURLInfo : undefined,wappalyzeResult : undefined});
-    setTimeout(async () => {
-      let basicInfo = await requestBasicURLInfo({url : text});
-      this.setState({analyzedURL : text,basicURLInfo : basicInfo});
-      setTimeout(async () => {
-        let wappalyzeResult = await requestWappalyze({url : text});
-        console.log(wappalyzeResult);
-        this.setState({wappalyzeResult : wappalyzeResult});
-      },50);
-    },50);
-  }
+    public onSearchUpdated = async (text : string) => {
+        console.log(text);
+        this.setState({analyzedURL : text,basicURLInfo : undefined,wappalyzeResult : undefined});
+        setTimeout(async () => {
+            let basicInfo = await requestBasicURLInfo({url : text});
+            this.setState({analyzedURL : text,basicURLInfo : basicInfo});
+            setTimeout(async () => {
+                let wappalyzeResult = await requestWappalyze({url : text});
+                console.log(wappalyzeResult);
+                this.setState({wappalyzeResult : wappalyzeResult});
+            },50);
+        },50);
+    }
 }
 
 export default App;
--- src/server.ts
+++ src/server.ts
@@ -18,71 +18,70 @@ app.use(bodyParser.urlencoded({extended: false}));
 const port = process.env.PORT || 5000;
 
 app.get('/basicURLInfo', async(req, res) => {
-  console.log(req.query);
-  if (!req.query) {
-    res.sendStatus(401);
-    return;
-  }
-  try {
-    let params = (<RequestBasicURLInfoParams>req.query);
-    let responseBody: BasicURLInfo = <any>{};
+    console.log(req.query);
+    if(!req.query)
+    {
+        res.sendStatus(401);
+        return;
+    }
+    try
+    {
+        let params = (<RequestBasicURLInfoParams>req.query);
+        let responseBody: BasicURLInfo = <any>{};
 
-    const browser = await puppeteer.launch({ignoreHTTPSErrors: true});
-    const {page, response, urls} =
+        const browser = await puppeteer.launch({ignoreHTTPSErrors: true});
+        const {page, response, urls} =
         await collectAllNetworkRequests(browser, params.url);
 
-    responseBody.urls = urls;
+        responseBody.urls = urls;
 
-    responseBody.remoteAddress = (<any>response).remoteAddress();
+        responseBody.remoteAddress = (<any>response).remoteAddress();
 
-    let screen = await page.screenshot({type: "jpeg", quality: 100});
+        let screen = await page.screenshot({type: "jpeg", quality: 100});
 
-    responseBody.screenShot = screen.toString("base64");
-    responseBody.srcText = await response.text();
+        responseBody.screenShot = screen.toString("base64");
+        responseBody.srcText = await response.text();
 
-    res.status(201);
-    res.json(responseBody);
-    await browser.close();
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(401);
-    return;
-  }
-
+        res.status(201);
+        res.json(responseBody);
+        await browser.close();
+    }
+    catch(err)
+    {
+        console.log(err);
+        res.sendStatus(401);
+        return;
+    }
 });
 
 app.get('/wappalyze',async(req,res) => {
     console.log(req.query);
-    if (!req.query) {
-      res.sendStatus(401);
-      return;
+    if(!req.query)
+    {
+        res.sendStatus(401);
+        return;
     }
-    try {
+    try
+    {
         let params = (<RequestWappalyzeParams>req.query);
         let responseBody : WappalyzeResult = await wappalyze(params.url);
         res.status(201);
         res.json(responseBody);
-    } catch (err) {
+    }
+    catch(err)
+    {
         console.log(err);
         res.sendStatus(401);
         return;
-      }
+    }
 });
 
-
-
 app.use(express.static(path.join(__dirname, '../build')));
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
-
 
 async function wappalyze(url : string) : Promise<WappalyzeResult>
 {
-    return new Promise<WappalyzeResult>(async (resolve : (value :
-WappalyzeResult) => void) => {
+    return new Promise<WappalyzeResult>(async (resolve : (value : WappalyzeResult) => void) => {
         const options = {
             debug: false,
             delay: 500,
@@ -93,37 +92,27 @@ WappalyzeResult) => void) => {
             userAgent: 'Wappalyzer',
             htmlMaxCols: 2000,
             htmlMaxRows: 2000,
-          };
-          const wappalyzer = new Wappalyzer(url,options);
-          let json = await wappalyzer.analyze();
-          return resolve(json);
+        };
+        const wappalyzer = new Wappalyzer(url,options);
+        let json = await wappalyzer.analyze();
+        return resolve(json);
     });
 }
 
-async function collectAllNetworkRequests(
-    browser: puppeteer.Browser, url: string): Promise<{
-  page: puppeteer.Page, response: puppeteer.Response, urls: Array<string>
-}> {
-  return new Promise<{
-    page: puppeteer.Page,
-    response: puppeteer.Response,
-    urls: Array<string>
-  }>(async(resolve: (val: {
-             page: puppeteer.Page,
-             response: puppeteer.Response,
-             urls: Array<string>
-           }) => void) => {
-    let urls = new Array<string>();
-    const page = await browser.newPage();
+async function collectAllNetworkRequests(browser: puppeteer.Browser, url: string): Promise<{page: puppeteer.Page, response: puppeteer.Response, urls: Array<string>}>
+{
+    return new Promise<{page : puppeteer.Page,response : puppeteer.Response,urls : Array<string>}>(async(resolve : (val : {page : puppeteer.Page,response : puppeteer.Response,urls : Array<string>}) => void) => {
+        let urls = new Array<string>();
+        const page = await browser.newPage();
 
+        page.on("request",(e: puppeteer.Request) => {
+            urls.push(e.url());
+        });
 
-    page.on("request", (e: puppeteer.Request) => { urls.push(e.url()); });
+        const req = await page.goto(url, {waitUntil: "networkidle2"});
 
-    const req = await page.goto(url, {waitUntil: "networkidle2"});
-
-    setTimeout(() => {
-      return resolve({page: page, response: req !, urls: urls});
-    }, 200);
-
-  });
+        setTimeout(() => {
+            return resolve({page: page, response: req !, urls: urls});
+        },200);
+    });
 }
